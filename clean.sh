fb55f4130615717373573425f453c38b639c32d6
Jeff <chien-hung.chen@case.edu>
2012-04-22 15:15:12 -0400

    committing readme changes and adding of .sh files

---
+++ clean.sh
@@ -0,0 +1,2 @@
+rm bin/edu/cwru/SimpleRTS/agent/*.class
+rm src/edu/cwru/SimpleRTS/agent/*.class
---
+++ run.sh
@@ -0,0 +1,3 @@
+javac -cp 'lib/SimpleRTSv3.1.jar'
+
+java -cp lib/SimpleRTSv3.1.jar:src edu.cwru.SimpleRTS.Main --config data/defaultConfig.xml data/rl_5fv5f.map --agent edu.cwru.SimpleRTS.agent.visual.VisualAgent 0 --agentparam true --agentparam true --agent  CombatAgent 1
